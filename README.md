130e3a2222dd4fff75110c4feccc5b2105a45588
Mario Lazzari <mario.lazzari@gmail.com>
2025-01-09 23:08:24 +0100

    Done lesson 26

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 
 24 - loading.tsx [video](https://www.youtube.com/watch?v=0OVg4ikUaz0)
 25 - error.tsx [video](https://www.youtube.com/watch?v=fWV5WPSbgdg) 
+26 - error restore [video](https://www.youtube.com/watch?v=15beQR0DFMQ)
--- routing-demo/src/app/products/[productId]/reviews/[reviewId]/error.tsx
+++ routing-demo/src/app/products/[productId]/reviews/[reviewId]/error.tsx
@@ -1,14 +1,28 @@
 "use client";
 
+import { useRouter } from "next/navigation";
+import { startTransition } from "react";
+
 type Props = {
   error: Error;
+  reset: () => void;
 };
 
-function ReviewErrorPage({ error }: Props) {
+function ReviewErrorPage({ error, reset }: Props) {
+  const router = useRouter();
+
+  const reload = () => {
+    startTransition(() => {
+      router.refresh();
+      reset();
+    });
+  };
+
   return (
     <>
       <h2>Error in review page</h2>
       <p>{error.message}</p>
+      <button onClick={reload}>Try Again</button>
     </>
   );
 }
