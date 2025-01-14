f18d6d1e39b81df5dfcead99f6b4244099e7ba2c
Mario Lazzari <mario.lazzari@gmail.com>
2025-01-14 08:50:52 +0100

    Done lesson 36

---
+++ route-handlers-demo/src/app/comments/data.ts
@@ -0,0 +1,14 @@
+export const comments = [
+  {
+    id: 1,
+    test: "First comment",
+  },
+  {
+    id: 2,
+    test: "Second comment",
+  },
+  {
+    id: 3,
+    test: "Third comment",
+  },
+];
