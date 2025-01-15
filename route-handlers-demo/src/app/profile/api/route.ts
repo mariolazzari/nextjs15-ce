d8df1d3699c2ca598f2e16296883c9d55167ef5a
Mario Lazzari <mario.lazzari@gmail.com>
2025-01-15 21:15:08 +0100

    Done lesson 43

--- route-handlers-demo/src/app/profile/api/route.ts
+++ route-handlers-demo/src/app/profile/api/route.ts
@@ -1,3 +1,25 @@
-export async function GET() {
-  return new Response("profile data");
+import { cookies, headers } from "next/headers";
+import { NextRequest } from "next/server";
+
+export async function GET(req: NextRequest) {
+  const reqHeaders = new Headers(req.headers);
+  let token = reqHeaders.get("Authorization");
+  console.log(token);
+
+  const headersList = await headers();
+  token = headersList.get("Authorization");
+  console.log(token);
+
+  const theme = req.cookies.get("theme");
+  console.log(theme);
+
+  const coookieStore = await cookies();
+  coookieStore.set("resultsPerPage", "10");
+
+  return new Response("<h1>Profile title</h1>", {
+    headers: {
+      "Content-Type": "text/html",
+      "Set-Cookie": "theme=dark",
+    },
+  });
 }
